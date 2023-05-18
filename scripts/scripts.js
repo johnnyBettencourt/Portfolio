89e57f49acbbf0e58c9b5fbc1018fb7be4cec215
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-05-18 07:22:30 -0700

    fixed contactModal not working

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#contactButton").click(function(){
+        $('#contactModal').modal('show')
+    });
+
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show')
+    });
+});
