a113750626716f3ec3656782de1b7f5891ea0e89
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-06-17 18:04:49 -0700

    fixed the card image for my portfolio card and changed the colors of the card discription

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,22 @@
-$(function() {
-    $("#contactButton").click(function(){
+$(function () {
+    $("#contactButton").click(function () {
         $('#contactModal').modal('show')
     });
 
-    $("#loginButton").click(function(){
+    $("#loginButton").click(function () {
         $('#loginModal').modal('show')
     });
+});
+
+var portfolioBtn = document.getElementById("portfolioInfoBtn");
+var i;
+
+portfolioBtn.addEventListener("click", () => {
+    this.classList.toggle("active");
+    var portfolioInfo = document.getElementById('portfolioInfo');
+    if (portfolioInfo.style.display != "none") {
+        portfolioInfo.style.display = "none";
+    } else {
+        portfolioInfo.style.display = "block";
+    }
 });
